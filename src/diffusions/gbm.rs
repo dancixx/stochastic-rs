7943000a52ff0f8e35399c71dd78819ddb181678
Daniel Boros <dancixx@gmail.com>
2023-05-29 22:24:27 +0200

    feat: add gbm support

--- src/diffusions/gbm.rs
+++ src/diffusions/gbm.rs
@@ -1,3 +1,32 @@
-pub fn gbm() {
-    todo!()
+use crate::noises::gn::gn;
+use nalgebra::DVector;
+
+pub fn gbm(mu: f64, sigma: f64, n: usize, t: Option<usize>, x0: Option<f64>) -> DVector<f64> {
+    let noise = gn(n - 1, t.unwrap_or(1));
+    let dt = t.unwrap_or(1) as f64 / n as f64;
+    let mut s = DVector::<f64>::zeros(n);
+    s[0] = x0.unwrap_or(100.0);
+
+    for i in 1..n {
+        let ds = (mu * dt + sigma * noise[i - 1]) * s[i - 1];
+        s[i] = s[i - 1] + ds;
+    }
+
+    s
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_gbm() {
+        let mu = 0.1;
+        let sigma = 0.2;
+        let n = 1000;
+        let t = 1;
+        let x0 = 100.0;
+        let gbm = gbm(mu, sigma, n, Some(t), Some(x0));
+        assert_eq!(gbm.len(), n);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use stochastic_rs::processes::{bm, fbm};
+use stochastic_rs::{
+    diffusions::gbm::gbm,
+    processes::{bm, fbm},
+};
 
 fn main() {
     let fbm = fbm::fbm_cholesky(10, 0.7, Some(2));
@@ -6,4 +9,7 @@ fn main() {
 
     let bm = bm::bm(10, None);
     println!("bm: {:?}", bm[0]);
+
+    let gbm = gbm(1.0, 0.5, 100, None, Some(10.0));
+    println!("gbm: {:?}", gbm);
 }
