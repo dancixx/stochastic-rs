75998e1f5f5c5b3573ed26a9fcfac6d48f1d34d9
Daniel Boros <dancixx@gmail.com>
2023-10-22 00:45:19 +0200

    fix: create and add fbm test

--- src/processes/bm.rs
+++ src/processes/bm.rs
@@ -5,7 +5,6 @@ use crate::noises::gn;
 pub fn bm(n: usize, t: Option<f64>) -> Vec<f64> {
     let gn = gn::gn(n - 1, t.unwrap_or(1.0));
     let mut bm = Array1::<f64>::zeros(n);
-    bm[0] = 0.0;
 
     for i in 1..n {
         bm[i] = bm[i - 1] + gn[i - 1];
--- src/processes/fbm.rs
+++ src/processes/fbm.rs
@@ -2,7 +2,7 @@ use crate::{
     noises::{fgn_cholesky, fgn_fft},
     utils::NoiseGenerationMethod,
 };
-use nalgebra::RowDVector;
+use ndarray::Array1;
 
 pub fn fbm(
     hurst: f64,
@@ -14,13 +14,37 @@ pub fn fbm(
         NoiseGenerationMethod::Fft => fgn_fft::fgn(hurst, n, t.unwrap_or(1.0)),
         NoiseGenerationMethod::Cholesky => fgn_cholesky::fgn(hurst, n - 1, t.unwrap_or(1.0)),
     };
-
-    let mut fbm = RowDVector::<f64>::zeros(n);
-    fbm[0] = 0.0;
+    let mut fbm = Array1::<f64>::zeros(n);
 
     for i in 1..n {
         fbm[i] = fbm[i - 1] + fgn[i - 1];
     }
 
-    fbm.data.as_vec().clone()
+    fbm.to_vec()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::statistics::fractal_dim::higuchi_fd;
+    use crate::utils::NoiseGenerationMethod;
+
+    #[test]
+    fn test_fbm() {
+        let (hurst, n) = (0.7, 10000);
+
+        let path = fbm(hurst, n, None, Some(NoiseGenerationMethod::Fft));
+        assert_eq!(path.len(), n);
+        assert_eq!(path[0], 0.0);
+
+        let h = higuchi_fd(&path, 10);
+        println!("Higuchi FD: {}", h);
+        assert!((2.0 - h) < 10e-1);
+
+        let path = fbm(hurst, n, None, Some(NoiseGenerationMethod::Cholesky));
+        assert_eq!(path.len(), n);
+
+        let h = higuchi_fd(&path, 10);
+        assert!((2.0 - h) < 10e-1);
+    }
 }
