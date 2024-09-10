030c06af191495cbf2d2c1cefc805d1c8c78f26f
Daniel Boros <dancixx@gmail.com>
2024-09-10 19:11:07 +0200

    feat: make rand complex to multithread

--- src/main.rs
+++ src/main.rs
@@ -21,4 +21,14 @@ fn main() {
   let sum: f32 = runs.iter().sum();
   let average = sum / runs.len() as f32;
   println!("Average time: {}", average);
+
+  let start = std::time::Instant::now();
+  let fbm = Fbm::new(0.75, 10000, Some(1.0), Some(1000));
+  let data = fbm.sample_par();
+  println!("Data: {:?}", data);
+  let duration = start.elapsed();
+  println!(
+    "Time elapsed in expensive_function() is: {:?}",
+    duration.as_secs_f32()
+  );
 }
